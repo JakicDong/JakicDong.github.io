8e305a487687e31737b6e419e353dedaa9591a5d
Hanya44 <2062966830@qq.com>
2025-06-05 20:23:49 +0800

    Site updated: 2025-06-05 20:23:49

--- func.js
+++ func.js
@@ -288,7 +288,7 @@ function getRandomColor() {
 
   function all(){
 	//这里插入checkedDays变量
-	checkedDays = {"2025-05-31":1,"2025-06-01":1};
+	checkedDays = {"2025-06-01":1,"2025-06-05":1};
 	currentYear=new Date().getFullYear();
 	//默认今年
 	get_elements();
